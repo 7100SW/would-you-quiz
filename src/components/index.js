4ce741b0505b4d3d18ea2059e6ced555c3787955
7100SW <seventy100@hotmail.com>
2021-02-13 11:20:14 -0800

    - Addition of material-ui - Implementation of a navigation prototype

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+export function Home() {
+    return <h2>Home</h2>;
+}
+
+export function About() {
+    return <h2>About</h2>;
+}
+
+export function Product() {
+    return <h2>Product</h2>;
+}
+
+export function Blog() {
+    return <h2>Blog</h2>;
+}
+
+export function Faq() {
+    return <h2>Frequently Asked Questions</h2>;
+}
+
+export function Contact() {
+    return <h2>Contact</h2>;
+}
